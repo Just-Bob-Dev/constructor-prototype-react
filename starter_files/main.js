aa5933f101f452dd5c67752156aebca4ae8a2bd6
Robert Hutchins <rjhutchins22@gmail.com>
2017-07-17 13:09:10 -0400

    finish assessment all tests pass

--- starter_files/main.js
+++ starter_files/main.js
@@ -7,14 +7,19 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog (name) {
+function Dog (name, color, hungry) {
+  this.name = name;
+  this.color = color;
+  this.hungry = hungry;
   this.satus = 'normal';
-  
+
 }
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
-
+let sadie = new Dog('sadie', 'black', false);
+let moonshine = new Dog('moonshine', 'white', true);
+let atticus = new Dog('atticus')
 //     __
 //    / /_  __  ______ ___  ____ _____  _____
 //   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
@@ -22,7 +27,19 @@ function Dog (name) {
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human () {}
+function Human (name, cool) {
+  this.name = name;
+  this.cool = cool;
+}
 
+Human.prototype.pet = function(dogName){
+  dogName.status = 'happy';
+}
+
+Human.prototype.feed = function(dogName){
+  dogName.hungry = false;
+}
 // Instances of Human
 // Needed: mason, julia
+let mason = new Human('mason', false);
+let julia = new Human('julia', true);
